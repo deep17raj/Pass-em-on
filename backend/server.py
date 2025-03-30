d47cda356e7c6e5064026dba8d9e3f79391528a8
aayushk231 <aayushkushwaha4@gmail.com>
2025-03-31 02:08:56 +0530

    Added upload notes, and search funtion

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, session, redirect, abort, request
-import os, pathlib, requests, google.auth.transport.requests
+import mysql.connector
+import os, pathlib, requests, google.auth.transport.requests, mysql.connector, db_cred
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 from pip._vendor import cachecontrol
@@ -9,7 +10,10 @@ os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" #https override for google oauth
 app = Flask("PassEmOn")
 app.secret_key = "secret"
 
-google_client_id = "436039800268-su7b5libe8shn70vuv19co49glgdvmog.apps.googleusercontent.com"
+IMGUR_UPLOAD_URL = 'https://api.imgur.com/3/image'
+CLIENT_ID = 'de0ac7ad6efd1aa'
+
+GOOGLE_CLIENT_ID = "436039800268-su7b5libe8shn70vuv19co49glgdvmog.apps.googleusercontent.com"
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json") #get the path to the client secret file
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file, 
@@ -35,11 +39,45 @@ def callback():
     cached_session = cachecontrol.CacheControl(request_session)
     token_request = google.auth.transport.requests.Request(session=cached_session)
 
-    id_info = id_token.verify_oauth2_token(id_token=credentials._id_token, request=token_request, audience=google_client_id)
+    id_info = id_token.verify_oauth2_token(id_token=credentials._id_token, request=token_request, audience=GOOGLE_CLIENT_ID)
     
     session["google_id"] = id_info.get("sub")
     session["name"] = id_info.get("name")
     return {"logged_in": 1, 
             "google_id": session["google_id"], 
             "name": session["name"],
+            "gmail_id": id_info.get("email")
             }
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    uid = ""
+    course = request.form['course_code']
+    slot = request.form['slot']
+    image = request.files['image']
+    files = {'image': image.read()}
+    headers = {'Authorization': 'Client-ID {}'.format(CLIENT_ID)}
+    
+    response = requests.post(IMGUR_UPLOAD_URL, headers=headers, files=files)
+    
+    if response.status_code == 200:
+        data = response.json()
+        link = data["data"]["link"]
+        mydb = mysql.connector.connect(host=db_cred.cred["host"], user=db_cred.cred["un"], password=db_cred.cred["pwd"], database=db_cred.cred["db"])
+        cursor = mydb.cursor()
+        query = "INSERT INTO NOTES VALUES {}, {}, {}, {}".format(uid, course, slot, link)
+        cursor.execute(query)
+        mydb.close()
+        return {data}
+    else:
+        return {data}
+
+@app.route("/search_page")
+def search_page():
+    search = request.args.get("q")
+    query = "SELECT * FROM NOTES WHERE course_code={}".format(search)
+    mydb = mysql.connector.connect(host=db_cred.cred["host"], user=db_cred.cred["un"], password=db_cred.cred["pwd"], database=db_cred.cred["db"])
+    cursor = mydb.cursor()
+    cursor.execute(query)
+    mydb.close()
+    return ""
